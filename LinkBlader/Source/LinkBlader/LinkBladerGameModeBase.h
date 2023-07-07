aabf7a75f4e12fe660e6680901786f9b9e5a546f
Bwasabe <jixg20@gmail.com>
2023-07-07 10:59:43 +0900

    Initialize

---
+++ LinkBlader/Source/LinkBlader/LinkBladerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LinkBladerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LINKBLADER_API ALinkBladerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
