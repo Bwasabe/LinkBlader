3b13efe69ffc51e00384adf9fcf5f8ca2b6511db
pilgyu14 <pilgue0822@naver.com>
2023-07-14 11:27:17 +0900

    0714_ai 제작중(BT, BB 생성()

---
+++ LinkBlader/Source/LinkBlader/Public/TrainingAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "TrainingAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LINKBLADER_API ATrainingAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
