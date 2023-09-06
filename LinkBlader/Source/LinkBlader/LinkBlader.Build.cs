1ced73cddbeff868325946dddd248d6286824fad
pilgyu14 <pilgue0822@naver.com>
2023-09-06 10:41:25 +0900

    Update LinkBlader.Build.cs

--- LinkBlader/Source/LinkBlader/LinkBlader.Build.cs
+++ LinkBlader/Source/LinkBlader/LinkBlader.Build.cs
@@ -8,7 +8,7 @@ public class LinkBlader : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "AIModule" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
