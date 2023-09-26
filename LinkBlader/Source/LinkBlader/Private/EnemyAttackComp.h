dd14a11789d459ee1253613eca344192ee021914
pilgyu14 <pilgue0822@naver.com>
2023-09-26 14:08:41 +0900

    0926_적 자체 스탯 구현

---
+++ LinkBlader/Source/LinkBlader/Private/EnemyAttackComp.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyBaseComponent.h"
+#include "EnemyAttackComp.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEnemyAttackComp : public UEnemyBaseComponent
+{
+	GENERATED_BODY()
+	
+public:
+	void MeleeAttack(); 
+};
--- LinkBlader/Source/LinkBlader/Private/TrainingCharacter.cpp
+++ LinkBlader/Source/LinkBlader/Private/TrainingCharacter.cpp
@@ -2,6 +2,7 @@
 
 
 #include "TrainingCharacter.h"
+#include "EnemyAttackComp.h"
 
 // Sets default values
 ATrainingCharacter::ATrainingCharacter()
@@ -9,13 +10,14 @@ ATrainingCharacter::ATrainingCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	enemyAttackComp = CreateDefaultSubobject<UEnemyAttackComp>(L"EnemyAttackComp");
 }
 
 // Called when the game starts or when spawned
 void ATrainingCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	 
+	AddComp(ModuleType::Attack, enemyAttackComp);
 }
 
 // Called every frame
@@ -25,10 +27,34 @@ void ATrainingCharacter::Tick(float DeltaTime)
 
 }
 
-// Called to bind functionality to input
-void ATrainingCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void ATrainingCharacter::AddComp(ModuleType _moduleType, UEnemyBaseComponent* _newComp)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	if (compMap.Contains(_moduleType) == false)
+	{
+		compMap.Add(_moduleType, _newComp);
+	}
 }
 
+template<typename T>
+T* ATrainingCharacter::GetComp(ModuleType _moduleType)
+{
+	T* _findModule = nullptr; 
+	if (compMap.Contains(_moduleType) == true)
+	{
+		_findModule = compMap.Find(_moduleType);
+	}
+	return _findModule; 
+}
+
+
+FString EnumToString(ModuleType EnumValue)
+{
+    UEnum* EnumPtr = FindObject<UEnum>(ANY_PACKAGE, TEXT("ModuleType"), true);
+
+    if (!EnumPtr)
+    {
+        return FString("Invalid");
+    }
+
+    return EnumPtr->GetNameStringByIndex(static_cast<int32>(EnumValue));
+}
